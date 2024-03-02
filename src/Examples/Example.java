0f8b6bda5edb08ba0fbbc20963ffb9e005962266
ShuriKenj1 <kgrichuk14@mail.ru>
2024-03-02 16:44:32 +0200

    Updated package Examples

--- src/Examples/Example.java
+++ src/Examples/Example.java
@@ -3,7 +3,7 @@ package Examples;
     Это пример простой программы на Java.
     Присвойте файлу с исходным кодом имя Example.java
  */
-class Example1 {
+class Example {
 // Выполнение любой программы на Java начинается
 // с вызова метода main()
     public static void main(String args[]) {
