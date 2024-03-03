70b3dfc4bcaab3c067473914b04135bf0f8f2c1f
ShuriKenj1 <kgrichuk14@mail.ru>
2024-03-03 19:17:42 +0200

    Committed Sound2.java

---
+++ src/Exercises/Sound2.java
@@ -0,0 +1,11 @@
+package Exercises;
+
+public class Sound2 {
+    public static void main(String[] args) {
+        double dist;
+
+        dist = 1100 * (7.2 / 2);
+        System.out.println("Расстояние до скалы составляет " +
+                dist + " футов");
+    }
+}
