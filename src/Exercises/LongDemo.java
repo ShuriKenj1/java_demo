5f489d93517a3c6f989113c1ea875a8dadaed128
ShuriKenj1 <kgrichuk14@mail.ru>
2024-03-03 16:07:42 +0200

    Committed LongDemo.java

---
+++ src/Exercises/LongDemo.java
@@ -0,0 +1,15 @@
+package Exercises;
+
+public class LongDemo {
+    public static void main(String[] args) {
+        long ci;
+        long im;
+
+        im = 5280 * 12;
+
+        ci = im * im * im;
+
+        System.out.println("В одной кубической миле содержится " + ci +
+                " кубических дюймов");
+    }
+}
