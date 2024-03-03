570db3f39b5b907048f01f901e2eb1fb4b2511d7
ShuriKenj1 <kgrichuk14@mail.ru>
2024-03-03 14:53:00 +0200

    Updated MyWeightOnMoon9.java

---
+++ src/Exercises/MyWeightOnMoon9.java
@@ -0,0 +1,13 @@
+package Exercises;
+
+public class MyWeightOnMoon9 {
+    public static void main(String args[]) {
+        int mywt;
+        double mywtOnMoon;
+
+        mywt = 78;
+        mywtOnMoon = mywt * 0.17;
+        System.out.println("My weight on Earth is " + mywt);
+        System.out.println("My weight on Moon is " + mywtOnMoon);
+    }
+}
