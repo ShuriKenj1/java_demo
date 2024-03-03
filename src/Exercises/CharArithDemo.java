2620daa01dd0f09e7c9ee62d0f560f9d6d7b296e
ShuriKenj1 <kgrichuk14@mail.ru>
2024-03-03 17:09:03 +0200

    Committed CharArithDemo.java

---
+++ src/Exercises/CharArithDemo.java
@@ -0,0 +1,17 @@
+package Exercises;
+
+public class CharArithDemo {
+    public static void main(String[] args) {
+        char ch;
+
+        ch = 'X';
+        System.out.println("ch содердит " + ch);
+
+        ch++; //инкрементировать (увеличение значения)
+              //переменную ch
+        System.out.println("теперь ch содержит " + ch);
+
+        ch = 90;
+        System.out.println("теперь ch содержит " + ch);
+    }
+}
