d5d455733dd084060b7d7e56b177e6f13111dffa
ShuriKenj1 <kgrichuk14@mail.ru>
2024-03-03 15:49:58 +0200

    Committed InchToMetres10.java

---
+++ src/Exercises/InchToMetres10.java
@@ -0,0 +1,26 @@
+package Exercises;
+
+public class InchToMetres10 {
+    public static void main(String[] args) {
+        double in, m;
+        int counter;
+    /*
+    1 ft = 12 in;
+    1 m = 39,37 in
+    надо вывести до 12 футов - 144 дюйма - в метры
+    */
+        counter = 0;
+
+        for(in = 1; in <= 144; in++){
+            m = in / 39.37; // преобразовал дюймы в метры
+            System.out.println(in + " дюйму соответсвует " +
+                    m + " метров");
+            counter++;
+            if (counter == 12) {
+                System.out.println();
+                counter = 0;
+            }
+        }
+    }
+}
+
