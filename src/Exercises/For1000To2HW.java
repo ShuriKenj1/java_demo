c4ff96f115c87190c526851bfc20bd4ede59e522
ShuriKenj1 <kgrichuk14@mail.ru>
2024-03-24 23:05:45 +0200

    Committed For1000To2HW.java

---
+++ src/Exercises/For1000To2HW.java
@@ -0,0 +1,12 @@
+package Exercises;
+
+public class For1000To2HW {
+    public static void main(String[] args) {
+
+        int x;
+
+        for (x = 1000; x >= 0; x -= 2) {
+            System.out.println(x);
+        }
+    }
+}
