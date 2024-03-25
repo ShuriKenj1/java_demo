337f788663205ba4a2be38d079341296620a8eb3
ShuriKenj1 <kgrichuk14@mail.ru>
2024-03-25 21:42:45 +0200

    Committed task 1 from the page 125

--- src/HW/Second/LookingForDotHW.java
+++ src/HW/Second/LookingForDotHW.java
@@ -1,16 +1,26 @@
-package Exercises;
+package HW.Second;
 
 import java.io.IOException;
 public class LookingForDotHW {
     public static void main(String[] args) throws IOException {
 
-        char ch, answer = '.';
+        char ch, ignore, answer = '.';
+        int spaceQuantity = 0;
 
-        System.out.println("Задумана буква из диапазона A-Z.");
-        System.out.print("Попытайтесь ее угадать: ");
+        do {
+            System.out.print("Print something until you type the dot: ");
 
-        ch = (char) System.in.read(); // получить символ с клавиатуры
+            ch = (char) System.in.read();
 
-        if (ch == answer) System.out.println("** Правильно! **");
+            do {
+                ignore = (char) System.in.read();
+            } while (ignore != '\n');
+            if (ch == ' ')
+                spaceQuantity++;
+            System.out.println("Current SPACE counter: " + spaceQuantity);
+            if (ch == answer) System.out.println("\nYou pressed the dot!\n" +
+                    "Total SPACE Counter: " + spaceQuantity);
+            else System.out.println("Try again\n");
+        } while (answer != ch);
     }
 }
