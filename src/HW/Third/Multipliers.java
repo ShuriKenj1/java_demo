5eccc2036971e0b68d34ec2674790ab98e2c4447
ShuriKenj1 <kgrichuk14@mail.ru>
2024-03-28 19:59:57 +0200

    Updated Multipliers.java, final (!!) version

--- src/HW/Third/Multipliers.java
+++ src/HW/Third/Multipliers.java
@@ -12,15 +12,15 @@ public class Multipliers {
         System.out.println("Your number is: ");
         int number = Integer.parseInt(input.nextLine());
 
-        for (i = number; i <= number; i++) {
+        for (i = number; i == number; i++) {
             if ((i % 2) == 0 | (i % 3) == 0) {
                 System.out.print("Multipliers of " + i + ": \n");
                 for (j = 2; j < i; j++) {
                     if ((i % j) == 0) {
-                        do {
                             k = i / j;
-                            System.out.println(j + " and " + k);
-                        } while (j <= k); // надо поменять, while не гибкий - либо убрать совсем do, либо поменять условие на более универсальное
+                            if (k >= j) {
+                                System.out.println(j + " and " + k);
+                            } else break;
                     }
                 }
             } else if ((i / 1) == i & (i / i) == 1) System.out.println("The number " + number + " has no multipliers, so it is a prime number." +
