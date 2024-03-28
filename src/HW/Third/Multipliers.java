9017b1f10f6a7cc3acea4c088454e2cd56fc6fa0
ShuriKenj1 <kgrichuk14@mail.ru>
2024-03-28 19:47:38 +0200

    Committed Multipliers.java, not final version

--- src/HW/Third/Multipliers.java
+++ src/HW/Third/Multipliers.java
@@ -2,21 +2,29 @@ package HW.Third;
 
 import java.util.Scanner;
 
-public class DividersOfNumber {
+public class Multipliers {
     public static void main(String[] args) {
+
+        int i, j, k;
 
         Scanner input = new Scanner(System.in);
 
         System.out.println("Your number is: ");
         int number = Integer.parseInt(input.nextLine());
 
-        for (int i = number; i <= number; i++) {
-            System.out.print("Dividers of " + i + ": ");
-            for (int j = 2; j <= i / 2; j++) {
-                if ((i % j) == 0) {
-                    System.out.print(j + " ");
+        for (i = number; i <= number; i++) {
+            if ((i % 2) == 0 | (i % 3) == 0) {
+                System.out.print("Multipliers of " + i + ": \n");
+                for (j = 2; j < i; j++) {
+                    if ((i % j) == 0) {
+                        do {
+                            k = i / j;
+                            System.out.println(j + " and " + k);
+                        } while (j <= k); // надо поменять, while не гибкий - либо убрать совсем do, либо поменять условие на более универсальное
+                    }
                 }
-            }
+            } else if ((i / 1) == i & (i / i) == 1) System.out.println("The number " + number + " has no multipliers, so it is a prime number." +
+                    "A prime number (or a prime) is a natural number greater than 1 that is not a product of two smaller natural numbers.");
         }
     }
 }
