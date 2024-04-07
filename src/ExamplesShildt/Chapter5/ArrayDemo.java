4bff8edd3fc5dc1a0032768e5a476b6f99b008a1
ShuriKenj1 <kgrichuk14@mail.ru>
2024-04-07 18:28:29 +0300

    Updated ArrayDemo.java

--- src/ExamplesShildt/Chapter5/ArrayDemo.java
+++ src/ExamplesShildt/Chapter5/ArrayDemo.java
@@ -5,11 +5,11 @@ public class ArrayDemo {
         int sample[] = new int[10];
         int i;
 
-        for (i = 0; i < 10; i = i+1){
+        for (i = 0; i < 10; i++){
             sample[i] = i;
         }
 
-        for (i = 0; i < 10; i = i + 1){
+        for (i = 0; i < 10; i++){
             System.out.println("Элемент sample[" + i + "]: " +
                     sample[i]);
         }
