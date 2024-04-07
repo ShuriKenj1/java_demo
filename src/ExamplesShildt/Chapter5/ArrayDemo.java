64df00e0dc4d0674b2162b37054976d90e77b11f
ShuriKenj1 <kgrichuk14@mail.ru>
2024-04-07 18:27:37 +0300

    Committed ArrayDemo.java

---
+++ src/ExamplesShildt/Chapter5/ArrayDemo.java
@@ -0,0 +1,17 @@
+package ExamplesShildt.Chapter5;
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+        int sample[] = new int[10];
+        int i;
+
+        for (i = 0; i < 10; i = i+1){
+            sample[i] = i;
+        }
+
+        for (i = 0; i < 10; i = i + 1){
+            System.out.println("Элемент sample[" + i + "]: " +
+                    sample[i]);
+        }
+    }
+}
