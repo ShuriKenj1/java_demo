099b405f8a8464551df4eb6772871b4493ebc4fe
ShuriKenj1 <kgrichuk14@mail.ru>
2024-04-25 21:37:07 +0300

    Committed CLDemo.java

---
+++ src/Lesson17/LinkedListDemo.java
@@ -0,0 +1,11 @@
+package Lesson17;
+
+import java.util.LinkedList;
+
+
+public class LinkedList {
+    public static void main(String[] args) {
+
+        LinkedList<Book> list = new LinkedList<>();
+    }
+}
