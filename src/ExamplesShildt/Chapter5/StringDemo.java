a3692d7cd56425a7b4f8f9aa36280f0ed4f6badc
ShuriKenj1 <kgrichuk14@mail.ru>
2024-04-25 13:37:16 +0300

    Committed StringDemo.java

---
+++ src/ExamplesShildt/Chapter5/StringDemo.java
@@ -0,0 +1,14 @@
+package ExamplesShildt.Chapter5;
+// Знакомство с классом String
+public class StringDemo {
+    public static void main(String[] args) {
+        // Различные способы объявления строк
+        String str1 = new String("В Java строки - объекты.");
+        String str2 = "Их можно создавать разными способами";
+        String str3 = new String(str2 + " и добавлять подобным образом дополнительные.");
+
+        System.out.println(str1);
+        System.out.println(str2);
+        System.out.println(str3);
+    }
+}
