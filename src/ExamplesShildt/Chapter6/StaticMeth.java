b8836d06378f7c95df1a6a8818c35d4131572bf8
ShuriKenj1 <kgrichuk14@mail.ru>
2024-05-01 19:17:10 +0300

    Committed StaticMeth.java

---
+++ src/ExamplesShildt/Chapter6/StaticMeth.java
@@ -0,0 +1,24 @@
+package ExamplesShildt.Chapter6;
+// Применение статического метода
+public class StaticMeth {
+    static int val = 1024; // статическая переменная
+
+    // Статический метод
+    static  int valDiv2(){
+        return val / 2;
+    }
+}
+
+class SDemo2 {
+    public static void main(String[] args) {
+
+        System.out.println("Значение val: " + StaticMeth.val);
+        System.out.println("StaticMeth.valDiv2(): " +
+                StaticMeth.valDiv2());
+
+        StaticMeth.val = 4;
+        System.out.println("Значение val: " + StaticMeth.val);
+        System.out.println("StaticMeth.valDiv2(): " +
+                StaticMeth.valDiv2());
+    }
+}
