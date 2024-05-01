a0cfa8abc46b809cb816ab23de4b2dac354cd3c8
ShuriKenj1 <kgrichuk14@mail.ru>
2024-05-01 19:17:57 +0300

    Added to Queue.java QDemo2 class and enforced Queue with new constructors

--- src/HW/Sixth/Queue.java
+++ src/HW/Sixth/Queue.java
@@ -2,14 +2,34 @@ package HW.Sixth;
 // Усовершенствованный класс очереди, предназначенной
 // для хранения символьных значений
 public class Queue {
-    // Эти члены класса теперь являются закрытыми
     private char q[]; // массив для хранения элементов очереди
     private int putloc, getloc; // индексы для вставки и извлечения
                                 // элементов очереди
 
+    // Сконструировать пустую очередь заданного размера
     Queue(int size){
         q = new char[size + 1]; // выделить память для очереди
         putloc = getloc = 0;
+    }
+
+    // Сконструировать очередь на основе имеющегося объекта Queue
+    Queue(Queue ob) {
+        putloc = ob.putloc;
+        getloc = ob.getloc;
+        q = new char[ob.q.length];
+
+        // Копировать элементы очереди
+        for (int i = getloc+1; i <= putloc; i++)
+            q[i] = ob.q[i];
+    }
+
+    // Сконструировать очередь на основе массива исходных значений
+    Queue (char a[]) {
+        putloc = 0;
+        getloc = 0;
+        q = new char[a.length + 1];
+
+        for (int i = 0; i < a.length; i++) put(a[i]);
     }
 
     // Поместить символ в очередь
@@ -32,5 +52,50 @@ public class Queue {
 
         getloc++;
         return q[getloc];
+    }
+}
+
+// Продемонстрировать использование класса Queue
+class QDemo2 {
+    public static void main(String[] args) {
+        // Создать пустую очередь для хранения 10 элементов
+        Queue q1 = new Queue(10);
+
+        char name[] = {'T', 'o', 'm'};
+        // Создать очередь на основе массива
+        Queue q2 = new Queue(name);
+
+        char ch;
+        int i;
+
+        // Поместить ряд символов в очередь q1
+        for (i = 0; i < 10; i++)
+            q1.put((char) ('A' + i));
+
+        // Создать одну очередь на основе другой
+        Queue q3 = new Queue(q1);
+
+        // Показать очереди
+        System.out.print("Содержимое q1: ");
+        for (i = 0; i < 10; i++) {
+            ch = q1.get();
+            System.out.print(ch);
+        }
+
+        System.out.println("\n");
+
+        System.out.print("Содержимое q2: ");
+        for (i = 0; i < 3; i++) {
+            ch = q2.get();
+            System.out.print(ch);
+        }
+
+        System.out.println("\n");
+
+        System.out.print("Содержимое q3: ");
+        for (i = 0; i < 10; i++) {
+            ch = q3.get();
+            System.out.print(ch);
+        }
     }
 }
