ad337561f109cc53f798bc946e54cc8e0cd77aec
ShuriKenj1 <kgrichuk14@mail.ru>
2024-05-26 22:04:48 +0300

    Client file - is done

--- src/HW/Sixth/BankSystem/Client.java
+++ src/HW/Sixth/BankSystem/Client.java
@@ -1,18 +1,39 @@
 package HW.Sixth.BankSystem;
 
+import com.sun.tools.javac.Main;
+
+import java.util.Random;
 import java.util.Scanner;
 
 public class Client {
     private String username;
     private String password;
-    private double ID;
-
-    Client() {
+    private int ID;
+    public Client() {
         setUsername();
         setPassword();
         setID();
+        MainMenu menu = new MainMenu(getID());
     }
-    public double getID() {
+    private void setID() {
+        Random rand = new Random();
+        ID =  rand.nextInt(900000) + 100000;
+    }
+    private void setUsername() {
+        System.out.println("Your name:");
+        Scanner scanner = new Scanner(System.in);
+        username = scanner.nextLine();
+    }
+    private void setPassword() {
+        System.out.println("Password:");
+        Scanner scanner0 = new Scanner(System.in);
+        password = scanner0.nextLine();
+//        if (password.length() < 6) {
+//            System.out.println("Sorry, your password is too short, try again.");
+//            setPassword();
+//        }
+    }
+    public int getID() {
         return ID;
     }
     public String getUsername() {
@@ -20,18 +41,5 @@ public class Client {
     }
     public String getPassword() {
         return password;
-    }
-    public void setID() {
-        ID = Math.random();
-    }
-    public void setUsername() {
-        System.out.println("Login:\n");
-        Scanner scanner = new Scanner(System.in);
-        username = scanner.nextLine();
-    }
-    public void setPassword() {
-        System.out.println("Password:\n");
-        Scanner scanner = new Scanner(System.in);
-        password = scanner.nextLine();
     }
 }
